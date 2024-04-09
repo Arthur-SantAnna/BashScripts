5e2b72a85a26f5f50fabe197861ad8d942b7e7f9
Arthur Carvalho <arthursantanna.c@gmail.com>
2024-04-09 15:35:45 -0300

    Create new filtered file using awk

--- atv04/covidssa.sh
+++ atv04/covidssa.sh
@@ -2,4 +2,13 @@
 
 ##Arthur Sant'Anna de Carvalho Santos##
 
-awk -F';' '{ print $3 }' /home/arthur/Downloads/HIST_PAINEL_COVIDBR_29mar2024/HIST_PAINEL_COVIDBR_2023_Parte2_29mar2024.csv
+ARQUIVO_CSV="HIST_PAINEL_COVIDBR_29mar2024/HIST_PAINEL_COVIDBR_2023_Parte2_29mar2024.csv"
+
+NOVO_CSV="covid_salvador_ba_2023_2.csv"
+
+awk -F';' 'NR==1 { print $2";" $3";" $8";" $12";" $14 }' "$ARQUIVO_CSV" > "$NOVO_CSV"
+
+awk -F';' '{ if ($2=="BA" && $3=="Salvador") print $2";" $3";" $8";" $12";" $14 }' "$ARQUIVO_CSV" >> "$NOVO_CSV"
+
+echo "Os dados para Salvador, BA, no segundo semestre de 2023 foram extraídos para o arquivo: $NOVO_CSV"
+
