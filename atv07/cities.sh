93e3dd0239ba3fd1c6b21ef4860a5de45af4ace8
Arthur Carvalho <arthursantanna.c@gmail.com>
2024-04-30 16:17:57 -0300

    Verify the day with the most new deaths for each city

--- atv07/cities.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ARQUIVO_CSV="HIST_PAINEL_COVIDBR_2023_Parte2_13abr2024.csv"
-
-awk -F';' '$2=="BA" && $3!="" {municipio=$3; cidade = municipio; obitosAcumulados=$13; obitosNovos=$14; data=$8; gsub(" ", "", cidade); if(!seen[municipio]){print "municipio;obitosAcumulados;obitosNovos;data" > cidade".csv"; seen[municipio] = 1;} print municipio";" obitosAcumulados";" obitosNovos";" data >> cidade".csv"}' "$ARQUIVO_CSV"
---
+++ atv07/max_novos_obitos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARQUIVO_CSV="HIST_PAINEL_COVIDBR_2023_Parte2_13abr2024.csv"
+
+NOVO_CSV="covid_ba_2023_2.csv"
+
+awk -F';' '$2=="BA" && $3!="" {municipio=$3; cidade = municipio; obitosAcumulados=$13; obitosNovos=$14; data=$8; gsub(" ", "", cidade); if(!seen[municipio]){print "municipio;obitosAcumulados;obitosNovos;data" > cidade".csv"; seen[municipio] = 1;} print municipio";" obitosAcumulados";" obitosNovos";" data >> cidade".csv"}' "$ARQUIVO_CSV"
+
+awk -F';' 'NR==1 { print $3";" $13";" "maxNovosObitos;" $8 }' "$ARQUIVO_CSV" > "$NOVO_CSV"
+
+for arq in *.csv
+do
+	if [ "$arq" != "$NOVO_CSV" ] && [ "$arq" != "$ARQUIVO_CSV" ]
+	then
+	awk -F';' 'NR==2 { municipio=$1; obitosAcumulados=$2; novosObitos=$3; data=$4; } NR>2 { if($3 >= novosObitos){ obitosAcumulados=$2; novosObitos=$3; data=$4 }}END{ print municipio";" obitosAcumulados";" novosObitos";" data }' "$arq" >> "$NOVO_CSV"
+	fi
+done
