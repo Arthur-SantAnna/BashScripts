c4a0dcf9c98c135553a2aa43f8c51cb2b66bb20f
Arthur Carvalho <arthursantanna.c@gmail.com>
2024-04-30 13:28:35 -0300

    Update script to create a .csv file for each city in Bahia

--- atv07/cities.sh
+++ atv07/cities.sh
@@ -2,4 +2,4 @@
 
 ARQUIVO_CSV="HIST_PAINEL_COVIDBR_2023_Parte2_13abr2024.csv"
 
-awk -F';' '$2=="BA" && $3!="" {municipio=$3; obitosAcumulados=$13; data=$8;}{print municipio; print obitosAcumulados; print data}' "$ARQUIVO_CSV"
+awk -F';' '$2=="BA" && $3!="" {municipio=$3; cidade = municipio; obitosAcumulados=$13; obitosNovos=$14; data=$8; gsub(" ", "", cidade); if(!seen[municipio]){print "municipio;obitosAcumulados;obitosNovos;data" > cidade".csv"; seen[municipio] = 1;} print municipio";" obitosAcumulados";" obitosNovos";" data >> cidade".csv"}' "$ARQUIVO_CSV"
